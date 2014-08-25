d0d0de6fcdde855ce1c313431b4f6dfc10eb2087
milanzikic <zikic.milan@gmail.com>
2014-08-25 16:09:05 +0200

    Member class
    Added class for Logged in member

---
+++ MedicinskaInformatika/HealthOnline/App_Code/Member.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+/// <summary>
+/// Summary description for Member
+/// </summary>
+public class Member
+{
+
+#region properties
+
+    enum MemberTypes
+    {
+        User = 1,
+        Admin = 2,
+        God = 99
+    };
+
+    private int memberId;
+    private int memberType;
+    private string memberName;
+    private string memberSurname;
+    private string memberUsername;
+    
+
+    protected int MemberId {
+        get { return this.memberId; }
+        set { this.memberId = value; }
+    }
+
+    protected int MemberType {
+        get { return this.memberType; }
+        set { this.memberType = value; }
+    }
+
+    protected string MemberName {
+        get { return this.memberName; }
+        set { this.memberName = value; }
+    }
+
+    protected string MemberSurname {
+        get { return this.memberSurname; }
+        set { this.memberSurname = value; }
+    }
+
+    protected string MemberUsername {
+        get { return this.memberUsername; }
+        set { this.memberUsername = value; }
+    }
+#endregion
+
+	public Member()
+	{
+		//
+		// TODO: Add constructor logic here
+		//
+	}
+    public Member(int memberType) 
+    {
+        //switch(memberType){
+        //    case MemberTypes.User: this.MemberType = MemberTypes.User;
+        //}    
+
+    }
+}
